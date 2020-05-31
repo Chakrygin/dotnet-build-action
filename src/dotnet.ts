eac03086c1fb060016ab249da768aaa24a68d6ef
Igor Chakrygin <igor@chakrygin.ru>
2020-05-31 23:01:04 +0300

    Initial version

---
+++ src/dotnet.ts
@@ -0,0 +1,118 @@
+import { exec } from '@actions/exec'
+
+export async function version() {
+  var result = '';
+
+  await exec('dotnet', ['--version'], {
+    silent: true,
+    listeners: {
+      stdout: buffer => {
+        result += buffer.toString();
+      }
+    }
+  });
+
+  return result.trim();
+}
+
+export interface RestoreOptions {
+  packages?: string;
+}
+
+export async function restore(project: string, options: RestoreOptions = {}) {
+  const args = ['restore', project];
+
+  if (options.packages) {
+    args.push('--packages', options.packages);
+  }
+
+  await exec('dotnet', args);
+}
+
+export interface BuildOptions {
+  configuration?: string;
+}
+
+export async function build(project: string, options: BuildOptions) {
+  const args = ['build', project];
+
+  args.push('--nologo');
+  args.push('--no-restore');
+
+  if (options.configuration) {
+    args.push('--configuration', options.configuration);
+  }
+
+  await exec('dotnet', args);
+}
+
+export interface TestOptions {
+  configuration?: string;
+  logger?: string;
+  resultsDirectory?: string;
+}
+
+export async function test(project: string, options: TestOptions) {
+  const args = ['test', project];
+
+  args.push('--nologo');
+  args.push('--no-build');
+
+  if (options.configuration) {
+    args.push('--configuration', options.configuration);
+  }
+
+  if (options.logger) {
+    args.push('--logger', options.logger);
+  }
+
+  if (options.resultsDirectory) {
+    args.push('--results-directory', options.resultsDirectory);
+  }
+
+  await exec('dotnet', args);
+}
+
+export interface PublishOptions {
+  configuration?: string;
+  output?: string;
+}
+
+export async function publish(project: string, options: PublishOptions) {
+  const args = ['publish', project];
+
+  args.push('--nologo');
+  args.push('--no-build');
+
+  if (options.configuration) {
+    args.push('--configuration', options.configuration);
+  }
+
+  if (options.output) {
+    args.push('--output', options.output);
+  }
+
+  await exec('dotnet', args);
+}
+
+export interface PackOptions {
+  configuration?: string;
+  output?: string;
+}
+
+export async function pack(project: string, options: PackOptions) {
+  const args = ['pack', project];
+
+  args.push('--nologo');
+  args.push('--no-build');
+
+  if (options.configuration) {
+    args.push('--configuration', options.configuration);
+  }
+
+  if (options.output) {
+    args.push('--output', options.output);
+  }
+
+  await exec('dotnet', args);
+}
---
+++ src/main.ts
@@ -0,0 +1,178 @@
+import * as core from '@actions/core'
+import * as glob from '@actions/glob'
+
+import * as fs from 'fs'
+import * as os from 'os'
+import * as path from 'path'
+
+import * as dotnet from './dotnet'
+
+async function main() {
+  try {
+
+    console.log();
+
+    skipFirstTimeExperience();
+
+    const solution = await findSolution();
+    const projectsToUnitTesting = await findProjectsToUnitTesting();
+    const projectsToIntegrationTesting = await findProjectsToIntegrationTesting();
+    const projectsToPacking = await findProjectsToPacking();
+
+    const configuration = core.getInput('CONFIGURATION');
+
+    await core.group(`Restoring "${solution}"...`, async () => {
+      await dotnet.restore(solution, {
+        packages: 'packages'
+      });
+    });
+
+    console.log();
+
+    await core.group(`Building "${solution}"...`, async () => {
+      await dotnet.build(solution, {
+        configuration: configuration,
+      });
+    });
+
+    console.log();
+
+    for (const project of projectsToUnitTesting) {
+      await core.group(`Testing "${project}"...`, async () => {
+        await dotnet.test(project, {
+          configuration: configuration,
+          logger: 'trx',
+          resultsDirectory: 'TestResults',
+        });
+      });
+
+      console.log();
+    }
+
+    for (const project of projectsToIntegrationTesting) {
+      const projectName = path.basename(project, '.csproj');
+      await core.group(`Publishing "${project}"...`, async () => {
+        await dotnet.publish(project, {
+          configuration: configuration,
+          output: path.join('artifacts/tests', projectName),
+        });
+      });
+
+      console.log();
+    }
+
+    for (const project of projectsToPacking) {
+      await core.group(`Packing "${project}"...`, async () => {
+        await dotnet.pack(project, {
+          configuration: configuration,
+          output: 'artifacts',
+        });
+      });
+
+      console.log();
+    }
+
+  } catch (error) {
+    core.setFailed(error);
+  }
+}
+
+export async function skipFirstTimeExperience() {
+  const version = await dotnet.version();
+  const sentinelDir = path.join(os.homedir(), '.dotnet');
+  const sentinelFile = path.join(sentinelDir, version + '.dotnetFirstUseSentinel');
+
+  fs.mkdirSync(sentinelDir, { recursive: true });
+  fs.writeFileSync(sentinelFile, '');
+}
+
+async function findSolution() {
+  const patterns = core.getInput('SOLUTION');
+  const solutions = await find(patterns);
+
+  if (solutions.length == 0) {
+    throw new Error('No solution to restoring and building found.');
+  }
+
+  const s = solutions.length > 1 ? 's' : '';
+  core.info(`Solution${s} to restoring and building:`);
+
+  for (const solution of solutions) {
+    core.info(`    ${solution}`);
+  }
+
+  console.log();
+
+  if (solutions.length > 1) {
+    throw new Error('Multiple solutions to restoring and building found.');
+  }
+
+  return solutions[0];
+}
+
+async function findProjectsToUnitTesting() {
+  const patterns = core.getInput('PROJECTS_TO_UNIT_TESTING');
+  const projects = await find(patterns);
+
+  if (projects.length > 0) {
+    const s = projects.length > 1 ? 's' : '';
+    core.info(`Project${s} to unit testing:`);
+
+    for (const project of projects) {
+      core.info(`    ${project}`);
+    }
+
+    console.log();
+  }
+
+  return projects;
+}
+
+async function findProjectsToIntegrationTesting() {
+  const patterns = core.getInput('PROJECTS_TO_INTEGRATION_TESTING');
+  const projects = await find(patterns);
+
+  if (projects.length > 0) {
+    const s = projects.length > 1 ? 's' : '';
+    core.info(`Project${s} to integration testing:`);
+
+    for (const project of projects) {
+      core.info(`    ${project}`);
+    }
+
+    console.log();
+  }
+
+  return projects;
+}
+
+async function findProjectsToPacking() {
+  const patterns = core.getInput('PROJECTS_TO_PACKING');
+  const projects = await find(patterns);
+
+  if (projects.length == 0) {
+    throw new Error('No projects to packing found.');
+  }
+
+  const s = projects.length > 1 ? 's' : '';
+  core.info(`Project${s} to packing:`);
+
+  for (const project of projects) {
+    core.info(`    ${project}`);
+  }
+
+  console.log();
+
+  return projects;
+}
+
+async function find(patterns: string) {
+  const globber = await glob.create(patterns);
+  const files = await globber.glob();
+
+  return files.map(file => {
+    return path.relative(process.cwd(), file);
+  });
+}
+
+main();
